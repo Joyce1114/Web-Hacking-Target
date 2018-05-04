116566fd25c11be7804ccee483fc6cc447653d14
kaiyim <kyle01ma@gmail.com>
2018-05-04 11:52:41 +0800

    add reflected XSS code

---
+++ reflectXSS.php
@@ -0,0 +1,23 @@
+<html>
+	<head>
+	</head>
+	
+	<body>
+		<form action="reflectXSS.php" method="get">
+			name: <input type="text" name="name" />
+			<input type="submit" value="submit" />
+		</form>
+		<br />
+		I am your <b>father</b>, 
+		<?php
+			$name = $_GET["name"];
+			echo strtoupper($name);
+		?>
+		<br />
+		<br />
+		<ol>
+			<li> Join the dark side.</li>
+			<li> Die horrible death.</li>
+		</ol>
+	</body>
+</html>
